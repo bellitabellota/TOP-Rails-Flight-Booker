a7d3787957a626050f97e874dd36a87a0eb7ae61
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-10-21 15:24:09 +0100

    Create search form and display flights on flights#index

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,6 @@
 class FlightsController < ApplicationController
-  def new
+  def index
+    @airport_options = Airport.all.map { |a| [ a.location, a.id ] }
+    @flight_selection = Flight.where(departure_airport_id: params[:departure_airport]).where(arrival_airport_id: params[:arrival_airport])
   end
 end
