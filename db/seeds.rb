5ad7fe9e4dbe22c54b62fe3fa606197b506febfc
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-10-21 10:10:53 +0100

    Add sample airport data in 'db/seeds.rb' and load with 'rails db:seed'

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+airport_list = [
+  [ "London", "LHR" ],
+  [ "Paris", "CDG" ],
+  [ "Frankfurt", "FRA" ],
+  [ "Amsterdam", "AMS" ],
+  [ "Madrid", "MAD" ],
+  [ "Barcelona", "BCN" ],
+  [ "Munich", "MUC" ],
+  [ "Rom", "FCO" ],
+  [ "Dublin", "DUB" ],
+  [ "Vienna", "VIE" ],
+  [ "Manchester", "MAN" ],
+  [ "Palma de Mallorca", "PMI" ],
+  [ "Oslo", "OSL" ],
+  [ "Istanbul", "IST" ],
+  [ "Stockholm", "ARN" ],
+  [ "Copenhagen", "CPH" ]
+]
+
+airport_list.each do |airport|
+  Airport.create(location: airport[0], code: airport[1])
+end
