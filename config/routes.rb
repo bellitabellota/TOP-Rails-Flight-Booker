0c07d687c3d045dbfb75557e49bb5eaaba9a26c2
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-10-21 15:13:56 +0100

    Configure route for '/search'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: "flights#index"
-  get "flights/index"
+
+  resources :flights, only: [ :index ]
+
+  get "/search", to: "flights#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
