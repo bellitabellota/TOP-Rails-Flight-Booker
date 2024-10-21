e1cfad7fb6faf7996f18cc33fe01b0c72b2c1c5a
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-10-21 12:56:55 +0100

    Seed db with flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,22 +11,34 @@
 airport_list = [
   [ "London", "LHR" ],
   [ "Paris", "CDG" ],
-  [ "Frankfurt", "FRA" ],
-  [ "Amsterdam", "AMS" ],
-  [ "Madrid", "MAD" ],
-  [ "Barcelona", "BCN" ],
-  [ "Munich", "MUC" ],
-  [ "Rom", "FCO" ],
-  [ "Dublin", "DUB" ],
-  [ "Vienna", "VIE" ],
-  [ "Manchester", "MAN" ],
-  [ "Palma de Mallorca", "PMI" ],
-  [ "Oslo", "OSL" ],
-  [ "Istanbul", "IST" ],
-  [ "Stockholm", "ARN" ],
-  [ "Copenhagen", "CPH" ]
+  [ "Frankfurt", "FRA" ]
 ]
 
 airport_list.each do |airport|
   Airport.create(location: airport[0], code: airport[1])
+end
+
+flight_list = [
+  [ "LHR", "CDG", "2025-06-07 10:00", 63 ],
+  [ "LHR", "CDG", "2025-06-15 17:00", 63 ],
+  [ "LHR", "CDG", "2025-06-20 22:00", 63 ],
+  [ "LHR", "FRA", "2025-06-07 10:00", 114 ],
+  [ "LHR", "FRA", "2025-06-20 12:00", 114 ],
+  [ "LHR", "FRA", "2025-06-27 20:00", 114 ],
+  [ "CDG", "LHR", "2025-06-07 16:00", 63 ],
+  [ "CDG", "LHR", "2025-06-24 13:00", 63 ],
+  [ "CDG", "LHR", "2025-06-27 06:00", 63 ],
+  [ "CDG", "FRA", "2025-06-27 06:00", 109 ],
+  [ "CDG", "FRA", "2025-06-05 07:30", 109 ],
+  [ "CDG", "FRA", "2025-06-19 17:00", 109 ],
+  [ "FRA", "CDG", "2025-06-02 10:00", 106 ],
+  [ "FRA", "CDG", "2025-06-09 11:00", 106 ],
+  [ "FRA", "CDG", "2025-06-15 13:00", 106 ],
+  [ "FRA", "LHR", "2025-06-10 13:50", 124 ],
+  [ "FRA", "LHR", "2025-06-18 15:45", 124 ],
+  [ "FRA", "LHR", "2025-06-23 16:10", 124 ]
+]
+
+flight_list.each do |flight|
+  Flight.create(departure_airport_id: Airport.find_by(code: flight[0]).id, arrival_airport_id: Airport.find_by(code: flight[1]).id, start_datetime: flight[2].to_datetime, duration_minutes: flight[3])
 end
