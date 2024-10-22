f52b4bbc422c2e2b4a37402755a1cf602e1a0de0
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-10-22 19:38:51 +0100

    Display only unique dates in the dropdown and only display flights with selected departure date

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,13 +1,14 @@
 class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.map { |a| [ a.location ] }
-    @flight_dates = Flight.all.map { |f| [ f.start_datetime.strftime("%d/%m/%Y") ] }
+    @flight_dates = Flight.all.map { |f| [ f.start_datetime.to_date.strftime("%d/%m/%Y") ] }.uniq
 
     if params.include?("departure_airport")
         @selected_departure_airport = Airport.find_by(location: params[:departure_airport])
         @selected_arrival_airport = Airport.find_by(location: params[:arrival_airport])
         @flight_selection = Flight.where(departure_airport_id: @selected_departure_airport.id)
                                   .where(arrival_airport_id: @selected_arrival_airport.id)
+                                  .where(start_datetime: params[:flight_date].to_date.beginning_of_day..params[:flight_date].to_date.end_of_day)
     end
   end
 end
