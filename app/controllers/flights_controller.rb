aa1a7e22dc088182ded63208f494f6b5d868c916
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-10-22 16:21:56 +0100

    Add dropdown for flight_date and passenger_number, change url form submits to and modify array provided for options_for_select to not show id's in address line

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,13 @@
 class FlightsController < ApplicationController
   def index
-    @airport_options = Airport.all.map { |a| [ a.location, a.id ] }
-    @flight_selection = Flight.where(departure_airport_id: params[:departure_airport]).where(arrival_airport_id: params[:arrival_airport])
+    @airport_options = Airport.all.map { |a| [ a.location ] }
+    @flight_dates = Flight.all.map { |f| [ f.start_datetime.strftime("%d/%m/%Y") ] }
+
+    if params.include?("departure_airport")
+        @selected_departure_airport = Airport.find_by(location: params[:departure_airport])
+        @selected_arrival_airport = Airport.find_by(location: params[:arrival_airport])
+        @flight_selection = Flight.where(departure_airport_id: @selected_departure_airport.id)
+                                  .where(arrival_airport_id: @selected_arrival_airport.id)
+    end
   end
 end
