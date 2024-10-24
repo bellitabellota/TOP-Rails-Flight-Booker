f6caf75583cb5a83b12dde20db13ebc620155cca
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-10-24 14:28:55 +0100

    Add #new, #create and #show action to BookingsController

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,28 @@
 class BookingsController < ApplicationController
   def new
+    @selected_flight =  Flight.find(params[:flight])
+    @selected_number_passengers = params[:passenger_number].to_i
+    @booking = Booking.new()
+    @selected_number_passengers.times { @booking.passengers.build }###
+  end
+
+  def create
+    @booking = Booking.new(passenger_params)
+
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def passenger_params
+    params.require(:booking).permit(:number_passengers, :flight_id, passengers_attributes: [ :name, :email ])
   end
 end
