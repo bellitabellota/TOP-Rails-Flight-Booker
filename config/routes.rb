9a73ebd3d22be5205ab3f642472138733b6c0396
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-10-24 14:25:14 +0100

    Add RESTful routes #create and #show for bookings

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :flights, only: [ :index ]
 
-  resources :bookings, only: [ :new ]
+  resources :bookings, only: [ :new, :create, :show ]
 
   get "/search", to: "flights#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
