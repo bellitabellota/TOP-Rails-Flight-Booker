4d08e3d91000cfdfd903c3e1483eda802c7b3b88
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-11-05 11:35:59 +0000

    Create #confirm_booking action in PassengerMailer

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,9 @@
 class PassengerMailer < ApplicationMailer
+  default from: "booking-confirmation@yourflightbooker.com"
+
+  def confirm_booking
+    @user = params[:user]
+    @url = root_url
+    mail(to: @user.email, subject: "Your flight booking is confirmed!")
+  end
 end
