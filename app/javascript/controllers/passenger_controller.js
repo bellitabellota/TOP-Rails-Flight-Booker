5217fa5be19608eccc6bed3fca42b98bd883e836
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-11-24 15:55:57 +0000

    Update the passenger number in the bookings#new view

--- app/javascript/controllers/passenger_controller.js
+++ app/javascript/controllers/passenger_controller.js
@@ -1,10 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets =[ "source", "passengers"]
+    static targets =[ "source", "passengers", "passengerNumber"]
 
     add() {
         console.log("Add passenger")
+        
+        let passengerNumber = Number(this.passengerNumberTarget.innerHTML)
+        passengerNumber++
+        this.passengerNumberTarget.innerHTML = passengerNumber
+        
         const clone = this.sourceTarget.content.cloneNode(true);
         this.passengersTarget.append(clone)
     }
