9b0f33f3399b42ac76e4a57fbc1f88cdcf7f1d40
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-12-01 14:32:12 +0000

    Remove files generted for Minitest

--- test/mailers/previews/passenger_mailer_preview.rb
+++
@@ -1,6 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/passenger_mailer
-class PassengerMailerPreview < ActionMailer::Preview
-  def confirm_booking
-    PassengerMailer.with(user: Passenger.last).confirm_booking
-  end
-end
