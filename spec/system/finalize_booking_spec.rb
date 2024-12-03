f24aa083c3ceaebe8a1098ac40d4c382124d5af4
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-12-03 08:46:45 +0000

    Add more test cases to 'Finalize Booking'

--- spec/system/finalize_booking_spec.rb
+++ spec/system/finalize_booking_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "Finalize Booking", type: :system do
   context "booking a flight option with 2 passengers" do
-    it "displays booking confirmation" do
+    before do
       flight = create(:flight)
 
       visit new_booking_path(flight: flight, passenger_number: 2)
@@ -12,8 +12,19 @@ RSpec.describe "Finalize Booking", type: :system do
       fill_in "booking_passengers_attributes_1_name", with: "Alice"
       fill_in "booking_passengers_attributes_1_email", with: "alice@email.com"
       click_on "Finalize Booking"
+    end
 
+    it "displays booking confirmation" do
       expect(page).to have_content("Booking confirmation")
+    end
+
+    it "displays the departure airport of the flight associated with the booking" do
+      expect(page).to have_content("MyLocation")
+    end
+
+    it "displays the passenger names associated with the booking" do
+      expect(page).to have_content("John")
+      expect(page).to have_content("Alice")
     end
   end
 end
