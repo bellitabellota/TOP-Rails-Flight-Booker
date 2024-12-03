b23dcfe1aab100706a84dd81611748a34b584284
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-12-03 08:19:31 +0000

    Shorten 'Finalize Booking test'

--- spec/system/finalize_booking_spec.rb
+++ spec/system/finalize_booking_spec.rb
@@ -4,21 +4,8 @@ RSpec.describe "Finalize Booking", type: :system do
   context "booking a flight option with 2 passengers" do
     it "displays booking confirmation" do
       flight = create(:flight)
-      @flight_selection = [ flight ]
-      create(:flight2)
 
-      visit root_path
-
-      select "MyLocation", from: "departure_airport"
-      select "London", from: "arrival_airport"
-      select "06/10/2025", from: "flight_date"
-      select 2, from: "passenger_number"
-
-      click_on "Search"
-
-
-      choose("Select flight")
-      click_on "Book flight"
+      visit new_booking_path(flight: flight, passenger_number: 2)
 
       fill_in "booking_passengers_attributes_0_name", with: "John"
       fill_in "booking_passengers_attributes_0_email", with: "john@email.com"
